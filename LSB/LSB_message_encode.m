fc93b81086d3129f5426cb4bcda732583ab18963
SnoWHandS <dillon.heald@gmail.com>
2019-05-07 13:44:06 +0200

    Added varying frequency rolling motion to noise

--- LSB/LSB_message_encode.m
+++ LSB/LSB_message_encode.m
@@ -1,7 +1,7 @@
 close all;clc;
 
 %Specify how long the noise should be for
-Length_Of_Noise = 10;
+Length_Of_Noise = 4;
 No_Noise_Samples = Length_Of_Noise*48000;
 
 %Create 1 second of 2 channel brown noise
@@ -17,27 +17,55 @@ audiowrite('brownnoise.wav',brownNoise,48000);
 %%Time specifications:
    Fs = 48000;                   % samples per second
    dt = 1/Fs;                   % seconds per sample
-   StopTime = 10;             % seconds
+   StopTime = 4;             % seconds
    t = (0:dt:StopTime-dt)';     % seconds
    %%Sine wave:
    Fc = 0.25;                     % hertz
-   rollingTide = cos(2*pi*Fc*t) +1.25;
+   rollingTide_25 = sin(2*pi*Fc*t) +1.25;
    % Plot the signal versus time:
    figure;
-   plot(t,rollingTide);
+   plot(t,rollingTide_25);
    xlabel('time (in seconds)');
    title('Signal versus Time');
    zoom xon;
-   
+
+length(rollingTide_25)
+length(signlechannelbrown());
 %mod rolling sine with brown noise
-rollingBrownNoise = 0.006.*rollingTide.*signlechannelbrown();
+rollingBrownNoise_25 = 0.006.*rollingTide_25.*signlechannelbrown();
 
-subplot(2,1,1);
+%%Sine wave:
+StopTime = 2.85;             % seconds
+t = (0:dt:StopTime-dt)';     % seconds
+Fc = 0.35;                     % hertz
+rollingTide_35 = sin(2*pi*Fc*t) +1.25;
+length(rollingTide_35)
+length(signlechannelbrown());
+
+
+%Specify how long the noise should be for
+Length_Of_Noise = StopTime;
+No_Noise_Samples = Length_Of_Noise*48000;
+
+%Create 1 second of 2 channel brown noise
+signlechannelbrown = dsp.ColoredNoise('Color','brown','SamplesPerFrame',No_Noise_Samples,...
+    'NumChannels',1);
+
+%mod rolling sine with brown noise
+rollingBrownNoise_35 = 0.006.*rollingTide_35.*signlechannelbrown();
+
+subplot(3,1,1);
 plot(brownNoise);
-subplot(2,1,2)
-plot(rollingBrownNoise);
+subplot(3,1,2)
+plot(rollingBrownNoise_25);
+subplot(3,1,3)
+plot(rollingBrownNoise_35);
+    
+finalNoise = [rollingBrownNoise_35; rollingBrownNoise_25];
+figure;
+plot(finalNoise);
 
-audiowrite('rollingbrownnoise.wav',rollingBrownNoise,48000);
+audiowrite('rollingbrownnoise_diff_freq.wav',finalNoise,48000);
 
 %Input a message
 message = fileread('message_to_encode.txt');
