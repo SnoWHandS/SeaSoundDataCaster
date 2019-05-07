83ca3f7efe3c532d50f2649b583445aa5cdb0cfd
Kali Singh <kalium@awful.cloud>
2019-05-07 09:59:30 +0200

    proof of concept filter

--- notch/mknotch.m
+++ notch/mknotch.m
@@ -5,10 +5,18 @@ y = (y(:,1));
 NFFT = 8000;
 Y = fftshift(fft(y, NFFT));
 fvals = fs*(-NFFT/2:NFFT/2-1)/NFFT;
-plot(fvals, abs(Y));
+% plot(fvals, abs(Y));
 
+y = y(1:22000);
 
+n6000 = notch_6000;
+n7000 = notch_7000;
 
+mono_6000 = filter(n6000, y);
+mono_7000 = filter(n7000, y);
+
+audiowrite('6000.wav', mono_6000, fs);
+audiowrite('7000.wav', mono_7000, fs);
 audiowrite('mono.wav', y, fs);
 
 % soundsc(y, fs);
